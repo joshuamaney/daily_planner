65535317dae0bf0bc3d81ac51ef8e70092b75bfd
Joshua Maney <joshuaraymaney@gmail.com>
2021-02-28 22:45:37 -0500

    deleted clear button, added save to local storage function

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,83 @@
+// clock and timer
 function update() {
     $('#currentDay').html(moment().format('D. MMMM YYYY h:mm:ss'));
   }
   
   setInterval(update, 1000);
+
+  // save to local storage
+  $("#savebtn9").on("click", function() {
+    
+    var input = $(this).siblings(".form-control").val();
+    var hour = $(this).siblings().attr("id");
+
+    localStorage.setItem(hour, input);
+  });
+
+  $("#savebtn10").on("click", function() {
+    
+    var input = $(this).siblings(".form-control").val();
+    var hour = $(this).siblings().attr("id");
+
+    localStorage.setItem(hour, input);
+  });
+
+  $("#savebtn11").on("click", function() {
+    
+    var input = $(this).siblings(".form-control").val();
+    var hour = $(this).siblings().attr("id");
+
+    localStorage.setItem(hour, input);
+  });
+
+  $("#savebtn12").on("click", function() {
+    
+    var input = $(this).siblings(".form-control").val();
+    var hour = $(this).siblings().attr("id");
+
+    localStorage.setItem(hour, input);
+  });
+
+  $("#savebtn1").on("click", function() {
+    
+    var input = $(this).siblings(".form-control").val();
+    var hour = $(this).siblings().attr("id");
+
+    localStorage.setItem(hour, input);
+  });
+
+  $("#savebtn2").on("click", function() {
+    
+    var input = $(this).siblings(".form-control").val();
+    var hour = $(this).siblings().attr("id");
+
+    localStorage.setItem(hour, input);
+  });
+
+  $("#savebtn3").on("click", function() {
+    
+    var input = $(this).siblings(".form-control").val();
+    var hour = $(this).siblings().attr("id");
+
+    localStorage.setItem(hour, input);
+  });
+
+  $("#savebtn4").on("click", function() {
+    
+    var input = $(this).siblings(".form-control").val();
+    var hour = $(this).siblings().attr("id");
+
+    localStorage.setItem(hour, input);
+  });
+
+  
+  
+  $("#clearbtn5").on("click", function() {
+    
+    var input = $(this).siblings(".form-control").val();
+    var hour = $(this).siblings().attr("id");
+
+    localStorage.clear(hour, input);
+  });
+
+  
